587a8eaae77968af2ee34b67a6038137838e8fbc
m3nf1s <titan94@mail.ru>
2018-11-17 23:53:36 +0400

    Задача 3

---
+++ CodeSignal/Task_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        /*Given the string, check if it is a palindrome.
+
+        Example
+
+        For inputString = "aabaa", the output should be checkPalindrome(inputString) = true;
+        For inputString = "abac", the output should be checkPalindrome(inputString) = false;
+        For inputString = "a", the output should be checkPalindrome(inputString) = true.*/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckPalindrome("abba"));
+            Console.WriteLine(CheckPalindrome("adfg"));
+
+        }
+
+        static bool CheckPalindrome(string inputString)
+        {
+            int count = 0;
+            for (int i = 0; i < inputString.Length; i++)
+                if (inputString[i] == inputString[inputString.Length - i - 1])
+                    count++;
+            if (count == inputString.Length)
+                return true;
+            return false;
+        }
+    }
+}
